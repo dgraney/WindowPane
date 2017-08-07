29cac06c43bbd9dc81968d228b1123d5555ef5dc
Daniel Graney <Daniel.Graney@bausch.com>
2017-08-07 16:11:57 -0400

    Added some code behind a rich text box control area. Not finalized, but it's started

--- WindowPane/Message Boxes/MsgBox_RichTextBox.Designer.cs
+++ WindowPane/Message Boxes/MsgBox_RichTextBox.Designer.cs
@@ -28,11 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MsgBox_RichTextBox));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.BoldButton = new System.Windows.Forms.ToolStripButton();
+            this.ItalicsButton = new System.Windows.Forms.ToolStripButton();
+            this.UnderlineButton = new System.Windows.Forms.ToolStripButton();
             this.tableLayoutPanel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -40,24 +46,26 @@
             this.tableLayoutPanel1.AutoSize = true;
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.textBox2, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.toolStrip1, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 22F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(358, 156);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(358, 207);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.button1.Location = new System.Drawing.Point(280, 129);
+            this.button1.Location = new System.Drawing.Point(280, 180);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 24);
             this.button1.TabIndex = 2;
@@ -80,23 +88,68 @@
             // richTextBox1
             // 
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Location = new System.Drawing.Point(3, 25);
+            this.richTextBox1.Location = new System.Drawing.Point(3, 47);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(352, 98);
+            this.richTextBox1.Size = new System.Drawing.Size(352, 127);
             this.richTextBox1.TabIndex = 4;
             this.richTextBox1.Text = "";
+            this.richTextBox1.SelectionChanged += new System.EventHandler(this.richTextBox1_SelectionChanged);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BoldButton,
+            this.ItalicsButton,
+            this.UnderlineButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 22);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(358, 22);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // BoldButton
+            // 
+            this.BoldButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BoldButton.Image = ((System.Drawing.Image)(resources.GetObject("BoldButton.Image")));
+            this.BoldButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BoldButton.Name = "BoldButton";
+            this.BoldButton.Size = new System.Drawing.Size(23, 19);
+            this.BoldButton.Text = "toolStripButton1";
+            this.BoldButton.ToolTipText = "Bold";
+            // 
+            // ItalicsButton
+            // 
+            this.ItalicsButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ItalicsButton.Image = ((System.Drawing.Image)(resources.GetObject("ItalicsButton.Image")));
+            this.ItalicsButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ItalicsButton.Name = "ItalicsButton";
+            this.ItalicsButton.Size = new System.Drawing.Size(23, 19);
+            this.ItalicsButton.Text = "Italics";
+            // 
+            // UnderlineButton
+            // 
+            this.UnderlineButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.UnderlineButton.Image = ((System.Drawing.Image)(resources.GetObject("UnderlineButton.Image")));
+            this.UnderlineButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.UnderlineButton.Name = "UnderlineButton";
+            this.UnderlineButton.Size = new System.Drawing.Size(23, 19);
+            this.UnderlineButton.Text = "toolStripButton2";
+            this.UnderlineButton.ToolTipText = "Underline";
             // 
             // MsgBox_RichTextBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(358, 156);
+            this.ClientSize = new System.Drawing.Size(358, 207);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "MsgBox_RichTextBox";
             this.Text = "MsgBox_RichTextBox";
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,5 +161,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton BoldButton;
+        private System.Windows.Forms.ToolStripButton ItalicsButton;
+        private System.Windows.Forms.ToolStripButton UnderlineButton;
     }
 }
--- WindowPane/Message Boxes/MsgBox_RichTextBox.cs
+++ WindowPane/Message Boxes/MsgBox_RichTextBox.cs
@@ -14,6 +14,10 @@ namespace WindowPane
     {
         public string returnRTF = "";
         public string returnText = "";
+
+        private bool isBold = false;
+        private bool isItalic = false;
+        private bool isUnderline = false;
         internal MsgBox_RichTextBox(string message, string caption, string buttonText)
         {
             InitializeComponent();
@@ -21,13 +25,115 @@ namespace WindowPane
             textBox2.Text = message;
             this.Text = caption;
             button1.Text = buttonText;
+
+            #region ToolStrip Settings
+            BoldButton.CheckOnClick = true;
+            BoldButton.CheckedChanged += BoldButton_CheckedChanged;
+
+            ItalicsButton.CheckOnClick = true;
+            ItalicsButton.CheckedChanged += ItalicsButton_CheckedChanged;
+
+            UnderlineButton.CheckOnClick = true;
+            UnderlineButton.CheckedChanged += UnderlineButton_CheckedChanged;
+            #endregion
         }
+
+        #region Functions
+        private void DoFontStyleChecks()
+        {
+            Font font = null;
+
+            if (isBold && isItalic && isUnderline)
+            { font = new Font(richTextBox1.Font, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline); SetFontStyle(richTextBox1, FontStyle.Bold | FontStyle.Italic | FontStyle.Underline); }
+            else if (isUnderline && isItalic)
+            { font = new Font(richTextBox1.Font, FontStyle.Underline | FontStyle.Italic); SetFontStyle(richTextBox1, FontStyle.Underline | FontStyle.Italic); }
+            else if (isBold && isUnderline)
+            { font = new Font(richTextBox1.Font, FontStyle.Bold | FontStyle.Underline); SetFontStyle(richTextBox1, FontStyle.Bold | FontStyle.Underline); }
+            else if (isBold && isItalic)
+            { font = new Font(richTextBox1.Font, FontStyle.Bold | FontStyle.Italic); SetFontStyle(richTextBox1, FontStyle.Bold | FontStyle.Italic); }
+            else if (isBold)
+            { font = new Font(richTextBox1.Font, FontStyle.Bold); SetFontStyle(richTextBox1, FontStyle.Bold); }
+            else if (isItalic)
+            { font = new Font(richTextBox1.Font, FontStyle.Italic); SetFontStyle(richTextBox1, FontStyle.Italic); }
+            else if (isUnderline)
+            { font = new Font(richTextBox1.Font, richTextBox1.Font.Style | FontStyle.Underline); SetFontStyle(richTextBox1, FontStyle.Underline); }
+            else
+            { font = new Font(richTextBox1.Font, FontStyle.Regular); SetFontStyle(richTextBox1, FontStyle.Regular); }
+
+            richTextBox1.SelectionFont = font;
+        }
+
+        private void SetFontStyle(RichTextBox rtb, FontStyle style)
+        {
+            int selectionStart = rtb.SelectionStart;
+            int selectionLength = rtb.SelectionLength;
+            int selectionEnd = selectionStart + selectionLength;
+
+            // select a letter by letter and change font
+            for (int x = selectionStart; x < selectionEnd; ++x)
+            {
+                // select a single letter
+                rtb.Select(x, 1);
+                // Toggle font style of the selection
+                rtb.SelectionFont = new Font(rtb.SelectionFont, rtb.SelectionFont.Style ^ style);
+            }
+            rtb.Select(selectionStart, selectionLength);
+        }
+        #endregion
+
+        private void UnderlineButton_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (UnderlineButton.CheckState == CheckState.Checked)
+                isUnderline = true;
+            else isUnderline = false;
+
+            DoFontStyleChecks();
+
+        }
+
+        private void ItalicsButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (ItalicsButton.CheckState == CheckState.Checked)
+                isItalic = true;
+            else
+                isItalic = false;
+            DoFontStyleChecks();
+        }
+
+        private void BoldButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (BoldButton.CheckState == CheckState.Checked)
+                isBold = true;
+            else
+                isBold = false;
+            DoFontStyleChecks();
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
             returnRTF = richTextBox1.Rtf;
             returnText = richTextBox1.Text;
             this.DialogResult = DialogResult.OK;
             this.Close();
+        }
+
+        private void richTextBox1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.SelectionFont.Bold)
+                BoldButton.Checked = true;
+            else
+                BoldButton.Checked = false;
+            if (richTextBox1.SelectionFont.Italic)
+                ItalicsButton.Checked = true;
+            else
+                ItalicsButton.Checked = false;
+            if (richTextBox1.SelectionFont.Underline)
+                UnderlineButton.Checked = true;
+            else
+                UnderlineButton.Checked = false;
+
+            
         }
     }
 }
